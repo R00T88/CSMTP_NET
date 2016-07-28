04704827ee88a1cc93ea4c832ec5aea820ab42e9
sbertini <sbertini@ATECNICA19>
2016-07-28 16:34:39 +0200

    Aggiungere i file di progetto.

---
+++ CSMTP_NET/CommandLine/CmdOption.vb
@@ -0,0 +1,215 @@
+﻿Imports CommandLine
+
+Public Class CmdOption
+
+#Region "SMTPServer"
+    <[Option]("SMTPServer", Required:=True, HelpText:="SMTP server address")>
+    Public Property SMTPServer() As String
+        Get
+            Return _SMTPServer
+        End Get
+        Set(ByVal value As String)
+            _SMTPServer = value
+        End Set
+    End Property
+
+    Private _SMTPServer As String
+#End Region
+
+#Region "SMTPPort"
+    <[Option]("SMTPPort", Required:=True, HelpText:="SMTP server port")>
+    Public Property SMTPPort() As Integer
+        Get
+            Return _SMTPPort
+        End Get
+        Set(ByVal value As Integer)
+            _SMTPPort = value
+        End Set
+    End Property
+
+    Private _SMTPPort As Integer
+#End Region
+
+#Region "SMTPSecurity"
+    <[Option]("SMTPSecurity", Required:=True, HelpText:="SMTP server security")>
+    Public Property SMTPSecurity() As Integer
+        Get
+            Return _SMTPSecurity
+        End Get
+        Set(ByVal value As Integer)
+            _SMTPSecurity = value
+        End Set
+    End Property
+
+    Private _SMTPSecurity As Integer
+#End Region
+
+#Region "SMTPAuth"
+    <[Option]("SMTPAuth", Required:=True, HelpText:="SMTP server authentication")>
+    Public Property SMTPAuth() As Integer
+        Get
+            Return _SMTPAuth
+        End Get
+        Set(ByVal value As Integer)
+            _SMTPAuth = value
+        End Set
+    End Property
+
+    Private _SMTPAuth As Integer
+#End Region
+
+#Region "SMTPUser"
+    <[Option]("SMTPUser", HelpText:="SMTP username")>
+    Public Property SMTPUser() As String
+        Get
+            Return _SMTPUser
+        End Get
+        Set
+            _SMTPUser = Value
+        End Set
+    End Property
+
+    Private _SMTPUser As String
+#End Region
+
+#Region "SMTPPwd"
+    <[Option]("SMTPPwd", HelpText:="SMTP password")>
+    Public Property SMTPPwd() As String
+        Get
+            Return _SMTPPwd
+        End Get
+        Set(ByVal value As String)
+            _SMTPPwd = value
+        End Set
+    End Property
+
+    Private _SMTPPwd As String
+#End Region
+
+#Region "From"
+    <[Option]("From", Required:=True, HelpText:="Sender")>
+    Public Property From() As String
+        Get
+            Return _From
+        End Get
+        Set(ByVal value As String)
+            _From = value
+        End Set
+    End Property
+
+    Private _From As String
+#End Region
+
+#Region "To"
+    <[Option]("To", Required:=True, Separator:=";", HelpText:="Receiver")>
+    Public Property [To]() As IEnumerable(Of String)
+        Get
+            Return _To
+        End Get
+        Set(ByVal value As IEnumerable(Of String))
+            _To = value
+        End Set
+    End Property
+
+    Private _To As IEnumerable(Of String)
+#End Region
+
+#Region "CC"
+    <[Option]("CC", Separator:=";", HelpText:="CC")>
+    Public Property CC() As IEnumerable(Of String)
+        Get
+            Return _CC
+        End Get
+        Set(ByVal value As IEnumerable(Of String))
+            _CC = value
+        End Set
+    End Property
+
+    Private _CC As IEnumerable(Of String)
+#End Region
+
+#Region "BCC"
+    <[Option]("BCC", Separator:=";", HelpText:="BCC")>
+    Public Property BCC() As IEnumerable(Of String)
+        Get
+            Return _BCC
+        End Get
+        Set(ByVal value As IEnumerable(Of String))
+            _BCC = value
+        End Set
+    End Property
+
+    Private _BCC As IEnumerable(Of String)
+#End Region
+
+#Region "Subject"
+    <[Option]("Subject", Required:=True, HelpText:="Mail Subject")>
+    Public Property Subject() As String
+        Get
+            Return _Subject
+        End Get
+        Set
+            _Subject = Value
+        End Set
+    End Property
+
+    Private _Subject As String
+#End Region
+
+#Region "Body"
+    <[Option]("Body", Required:=True, HelpText:="Mail Body")>
+    Public Property Body() As String
+        Get
+            Return _Body
+        End Get
+        Set
+            _Body = Value
+        End Set
+    End Property
+
+    Private _Body As String
+#End Region
+
+#Region "Attachment"
+    <[Option]("Attachment", Separator:=";", HelpText:="Attachment files")>
+    Public Property Attachment() As IEnumerable(Of String)
+        Get
+            Return _Attachment
+        End Get
+        Set(ByVal value As IEnumerable(Of String))
+            _Attachment = value
+        End Set
+    End Property
+
+    Private _Attachment As IEnumerable(Of String)
+#End Region
+
+#Region "Urgent"
+    <[Option]("Urgent", HelpText:="Urgent flag")>
+    Public Property Urgent() As Integer
+        Get
+            Return _Urgent
+        End Get
+        Set(ByVal value As Integer)
+            _Urgent = value
+        End Set
+    End Property
+
+    Private _Urgent As Integer
+#End Region
+
+#Region "Log"
+    <[Option]("Log", HelpText:="Debug Log")>
+    Public Property Log() As String
+        Get
+            Return _Log
+        End Get
+        Set(ByVal value As String)
+            _Log = value
+        End Set
+    End Property
+
+    Private _Log As String
+#End Region
+
+End Class
---
+++ CSMTP_NET/SMTP_Main.vb
@@ -0,0 +1,129 @@
+﻿
+Imports System.IO
+Imports MailKit
+Imports MailKit.Net.Smtp
+Imports MimeKit
+
+Module SMTP_Main
+
+    Sub Main(ByVal szArgs() As String)
+
+        ' Intestazione
+        Console.ForegroundColor = ConsoleColor.Cyan
+        Console.WriteLine("***************************************")
+        Console.WriteLine("*              CSMTP.NET              *")
+        Console.WriteLine("***************************************")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine("")
+
+        ' Controllo parametri linea di comando
+        If szArgs.Length < 1 Then
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine("[!] - Nessun parametro passato")
+            Console.ForegroundColor = ConsoleColor.White
+            Console.WriteLine("")
+            Exit Sub
+        End If
+
+        ' Parsing parametri
+        Dim pResult = CommandLine.Parser.Default.ParseArguments(Of CmdOption)(szArgs)
+
+        ' Errore nel parsing
+        If pResult.Tag = CommandLine.ParserResultType.NotParsed Then
+            Exit Sub
+        End If
+
+        Dim pCommandResult As CommandLine.Parsed(Of CmdOption) = pResult
+
+        ' Impostazione messaggio
+        Dim pMessage = New MimeMessage()
+        Dim dwIndex As Integer
+
+        ' From
+        pMessage.From.Add(New MailboxAddress(pCommandResult.Value.From, pCommandResult.Value.From))
+
+        ' To
+        For dwIndex = 0 To pCommandResult.Value.To.Count - 1
+            pMessage.To.Add(New MailboxAddress(pCommandResult.Value.To(dwIndex).ToString, pCommandResult.Value.To(dwIndex).ToString))
+        Next
+
+        ' CC
+        For dwIndex = 0 To pCommandResult.Value.CC.Count - 1
+            pMessage.Cc.Add(New MailboxAddress(pCommandResult.Value.CC(dwIndex).ToString, pCommandResult.Value.CC(dwIndex).ToString))
+        Next
+
+        ' BCC
+        For dwIndex = 0 To pCommandResult.Value.BCC.Count - 1
+            pMessage.Bcc.Add(New MailboxAddress(pCommandResult.Value.BCC(dwIndex).ToString, pCommandResult.Value.BCC(dwIndex).ToString))
+        Next
+
+        ' Subject
+        pMessage.Subject = pCommandResult.Value.Subject
+
+        ' Body
+        Dim pBody = New TextPart("plain")
+        pBody.Text = pCommandResult.Value.Body
+
+        ' Attachment
+        If pCommandResult.Value.Attachment.Count > 0 Then
+
+            Dim pMultipart = New Multipart("mixed")
+            pMultipart.Add(pBody)
+
+            For dwIndex = 0 To pCommandResult.Value.Attachment.Count - 1
+
+                If Not File.Exists(pCommandResult.Value.Attachment(dwIndex)) Then
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine("[!] - " + pCommandResult.Value.Attachment(dwIndex) + " non trovato")
+                    Console.ForegroundColor = ConsoleColor.White
+                    Console.WriteLine("")
+                    Exit Sub
+                End If
+
+                Dim pAttachment = New MimePart(Path.GetFileName(pCommandResult.Value.Attachment(dwIndex)), Path.GetExtension(pCommandResult.Value.Attachment(dwIndex)))
+                pAttachment.ContentObject = New ContentObject(File.OpenRead(pCommandResult.Value.Attachment(dwIndex)), ContentEncoding.Default)
+                pAttachment.ContentDisposition = New ContentDisposition(ContentDisposition.Attachment)
+                pAttachment.ContentTransferEncoding = ContentEncoding.Base64
+                pMultipart.Add(pAttachment)
+
+            Next
+
+            pMessage.Body = pMultipart
+        Else
+            pMessage.Body = pBody
+        End If
+
+        Dim pClient As SmtpClient
+
+        ' Init SmtpClient
+        If pCommandResult.Value.Log <> Nothing Then
+            pClient = New SmtpClient(New ProtocolLogger(pCommandResult.Value.Log))
+        Else
+            pClient = New SmtpClient()
+        End If
+
+        Try
+            ' Connect
+            pClient.Connect(pCommandResult.Value.SMTPServer, pCommandResult.Value.SMTPPort, IIf(pCommandResult.Value.SMTPSecurity = 1, True, False))
+
+            ' Authenticate
+            If pCommandResult.Value.SMTPAuth = 1 Then
+                pClient.Authenticate(pCommandResult.Value.SMTPUser, pCommandResult.Value.SMTPPwd)
+            End If
+
+            ' Send
+            pClient.Send(pMessage)
+
+            ' disconnect
+            pClient.Disconnect(True)
+        Catch ex As Exception
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine("[!] - " + ex.Message)
+            Console.ForegroundColor = ConsoleColor.White
+            Console.WriteLine("")
+            Exit Sub
+        End Try
+
+    End Sub
+
+End Module
